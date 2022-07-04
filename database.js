49347c331125e3b309e47be3e91109bc6a66bfa2
Jirat Viriyataranon <jiratsun.11@gmail.com>
2022-07-05 00:23:25 +0700

    remove versionKey and use toJSON in schema

--- database.js
+++ database.js
@@ -5,12 +5,17 @@ mongoose.connect(
     { useNewUrlParser: true }
 );
 
-const noteSchema = new mongoose.Schema({
-    content: { type: String, required: [true, "content required"] },
-    comment: { type: String, default: "" },
-    status: { type: String, required: [true, "status required"] },
-    isFavorite: { type: Boolean, required: [true, "isFavorite required"] },
-    isCurrent: { type: Boolean, default: false },
-});
+const noteSchema = new mongoose.Schema(
+    {
+        content: { type: String, required: [true, "content required"] },
+        comment: { type: String, default: "" },
+        status: { type: String, required: [true, "status required"] },
+        isFavorite: { type: Boolean, required: [true, "isFavorite required"] },
+        isCurrent: { type: Boolean, default: false },
+    },
+    { versionKey: false }
+);
+
+noteSchema.set("toJSON", { virtuals: true });
 
 module.exports.Note = mongoose.model("Note", noteSchema);
